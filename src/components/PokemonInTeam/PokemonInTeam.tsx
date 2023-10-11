d3f6ce12b88110a0e936918573fbc5f208e073bd
Peter <pn7000@gmail.com>
2023-10-11 15:25:00 -0500

    Displays pokemon team

---
+++ src/components/PokemonInTeam/PokemonInTeam.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react'
+import "./PokemonInTeam.css"
+
+function PokemonInTeam(props: {pokemonName: String}) {
+    console.log(props)
+    //let loading = true
+    let [pokemon, setPokemon] = useState({} as any)
+    let [loading, setLoading] = useState(true)
+    
+    const pokeUrl = "https://pokeapi.co/api/v2/pokemon/"
+    let lower = props.pokemonName.toLowerCase()
+    useEffect(() => {
+        //fetch pokemon from pokeapi
+        fetch(`${pokeUrl}${lower}`)
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result)
+                setPokemon(result)
+                
+                setLoading(false)
+            }).catch((err)=> {
+                console.log(err)
+            })
+        }, [])
+    if (loading) {
+        return (<p>Loading...</p>)
+    } else {
+        return (
+        <div className='card'>
+            <h2>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.substr(1).toLowerCase()}</h2>
+            <div className='sprite'>
+                <img src={pokemon.sprites.front_default} alt="pokemon sprite"></img>
+            </div>
+        </div>
+    )
+    }
+}
+
+export default PokemonInTeam
--- src/components/TeamView/TeamView.tsx
+++ src/components/TeamView/TeamView.tsx
@@ -1,32 +1,46 @@
 import React, { useState} from 'react'
 import axios from 'axios'
 import './TeamView.css'
+import PokemonInTeam from '../PokemonInTeam/PokemonInTeam'
 
 function TeamView() {
     let [state, setState] = useState({
         loading: true,
         team_id: "",
-        team_name: "", 
+        team_name: "",
         pokemon_list: []
     })
+
 
     //test id
     const id = "806075bc-44be-4486-a2a2-86c85b929ab9" 
     const url = `http://localhost:3000/teams/${id}`
+
+    // var requestOptions = {
+    //     method: "GET",
+    //     redirect: "follow"
+    // }
+
     
     React.useEffect(() => {
-
-    axios.get(url).then((response) => {
+    
+        axios.get(url).then((response) => {
+                
+            console.log('Data: ', response.data);
+            const team = response.data.body
+            console.log('Team: ', team)
+            console.log("pokemon list: " + team.pokemonList)
             
-        console.log('Data: ', response.data);
-        const team = response.data.body
-        console.log('Team: ', team)
-        setState({...state, loading: false, team_id: team.team_id, team_name: team.name, pokemon_list: team.pokemonList})
             
-    }).catch((err) => {
+            setState({...state, loading: false, team_id: team.team_id, team_name: team.name, pokemon_list: team.pokemonList})
+            
+        }
+    ).catch((err) => {
         console.error('Error:', err);
     })
     }, [])
+
+    
     
       
     if (state.loading) {
@@ -34,12 +48,13 @@ function TeamView() {
     } else {
     return (
         <div>
-            <p>TEAMVIEW TEST</p>
-            <p>{state.team_name}</p>
-            <ul>
+            <h1>{state.team_name}</h1>
+            <div>
                 {state.pokemon_list.map((pokemon) => 
-                <p>{pokemon}</p>)}
-            </ul>
+                //pokemon get rendered here
+                <PokemonInTeam key={state.pokemon_list.indexOf(pokemon)} pokemonName={pokemon}/>
+                )}
+            </div>
         </div>
     )
     }

2ab7d973c06d7c33d061903ca774e86fd49ee647
moises sanchez <jossanchez@ucdavis.edu>
2023-10-12 08:15:14 -0700

    Josh's Update 1.0

--- src/Components/Post/Post.tsx
+++ src/Components/Post/Post.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../utility/reduxTypes'; // Import your RootState type
 import './Post.scss';
 //import { Event } from 'aws-sdk/clients/s3';
 function Post() {
   const AuthState = useSelector((state: RootState) => state.auth);
+  const [textareaValue, setTextareaValue] = useState('');
+
+  const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextareaValue(e.target.value);
+  };
   function on_hover_button(e : React.MouseEvent<HTMLButtonElement>) {
     e.currentTarget.style.background = '#034480';
   }
   function on_leave_button(e : React.MouseEvent<HTMLButtonElement>) {
     e.currentTarget.style.background = '#035096';
+  }
+  function on_click_submit(e: React.MouseEvent<HTMLButtonElement>){
+    console.log(textareaValue)
   }
 //credit https://css-tricks.com/auto-growing-inputs-textareas/ Chris Coyier on Mar 25, 2020
 function calcHeight(value: string) {
@@ -26,12 +34,12 @@ function on_keyup_textarea(e : React.KeyboardEvent<HTMLTextAreaElement>) {
         <span>is feeling okay</span>
         {/* Use the token as needed in this child component */}
         <br/>
-        <textarea placeholder="What's on your mind" id="post_body" onKeyUp={on_keyup_textarea}></textarea>
+        <textarea placeholder="What's on your mind" id="post_body" onKeyUp={on_keyup_textarea} value={textareaValue}
+        onChange={handleTextareaChange}></textarea>
         <br/>
         <button className="btn btn-info" id="upload_image">Upload Image</button>
-        <button className="btn btn-info" id="add_tags">Tags <img src="(../../../public/images/tag.png"/></button>
-
-        <button className="btn btn-info" id="post_button" onMouseOver={on_hover_button} onMouseLeave={on_leave_button}>Post</button>
+        <button className="btn btn-info" id="add_tags">Tags <img src= {require("../../images/tag.jpg")} alt ="" id="tag_img"/></button>
+        <button className="btn btn-info" id="post_button" onMouseOver={on_hover_button} onMouseLeave={on_leave_button} onClick={on_click_submit}>Post</button>
       </div>
   );
 }

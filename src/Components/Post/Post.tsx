81e66763405f5719ac91cf5b760c15cdb5218dfd
moises sanchez <jossanchez@ucdavis.edu>
2023-10-12 13:32:21 -0700

    fixed redux change mechanism

--- src/Components/Post/Post.tsx
+++ src/Components/Post/Post.tsx
@@ -3,20 +3,31 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../utility/reduxTypes'; // Import your RootState type
 import { useDispatch } from 'react-redux';
 import { setUserInfo } from '../../utility/auth'; // Im+
-import { AppDispatch } from '../../utility/store';
+import { AppDispatch } from '../../utility/store';//
+import { MultiSelect } from "react-multi-select-component";
 
 import './Post.scss';
 //import { Event } from 'aws-sdk/clients/s3';
+const tagoptions = [
+  { label: "#trades", value: "trades" },
+  { label: "#catches", value: "catches" },
+  { label: "#general", value: "general"},
+  { label: "#memes", value: "memes"},
+  { label: "#image", value: "image"},
+  { label: "#challenge", value: "challenge"},
+  { label: "#commentary", value: "commentary"},
+  { label: "#nsfw", value: "nsfw"},
+];
+
 function Post() {
   const AuthState = useSelector((state: RootState) => state.auth);
   const [textareaValue, setTextareaValue] = useState('');
   const dispatch: AppDispatch = useDispatch(); // Use AppDispatch for dispatching actions
-  const updatedInfo = {
-    name: "NOT_JOSH",
-    user_id: 'something reasonable', // You can update other fields as well
-    token: 'sha256--23',
-  };
-  dispatch(setUserInfo(updatedInfo));
+
+
+  
+
+
   const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTextareaValue(e.target.value);
   };
@@ -27,7 +38,13 @@ function Post() {
     e.currentTarget.style.background = '#035096';
   }
   function on_click_submit(e: React.MouseEvent<HTMLButtonElement>){
-    console.log(textareaValue)
+    console.log(textareaValue);
+    const updatedInfo = {
+      name: 'josh',
+      user_id: 'something reasonable', // You can update other fields as well
+      token: 'sha256--23',
+    };
+    dispatch(setUserInfo(updatedInfo));
   }
 //credit https://css-tricks.com/auto-growing-inputs-textareas/ Chris Coyier on Mar 25, 2020
 function calcHeight(value: string) {
@@ -38,20 +55,31 @@ function calcHeight(value: string) {
 function on_keyup_textarea(e : React.KeyboardEvent<HTMLTextAreaElement>) {
   e.currentTarget.style.height = calcHeight(e.currentTarget.value) + "px";
 }
-  return (
-      <div id = "post_container">
-        <span>{AuthState.name} </span>
-        <span>is feeling okay</span>
-        {/* Use the token as needed in this child component */}
-        <br/>
+
+const [selected, setSelected] = useState([]);
+return (
+    <div id = "post_container">
+      <span>{AuthState.name} </span>
+      <span>is feeling okay</span>
+      {/* Use the token as needed in this child component */}
+      <br/>
         <textarea placeholder="What's on your mind" id="post_body" onKeyUp={on_keyup_textarea} value={textareaValue}
         onChange={handleTextareaChange}></textarea>
-        <br/>
-        <button className="btn btn-info" id="upload_image">Upload Image</button>
-        <button className="btn btn-info" id="add_tags">Tags <img src= {require("../../images/tag.jpg")} alt ="" id="tag_img"/></button>
-        <button className="btn btn-info" id="post_button" onMouseOver={on_hover_button} onMouseLeave={on_leave_button} onClick={on_click_submit}>Post</button>
+      <br/>
+     
+      <button className="btn btn-info" id="upload_image">Upload Image</button>
+      <div id = "add_tags">
+        <h1 id = "tag_header">Tags <img src= {require("../../images/tag.jpg")} alt ="" id="tag_img"/></h1>
+        <MultiSelect
+          options={tagoptions}
+          value={selected}
+          onChange={setSelected}
+          labelledBy="Select"
+        />
       </div>
-  );
+      <button className="btn btn-info" id="post_button" onMouseOver={on_hover_button} onMouseLeave={on_leave_button} onClick={on_click_submit}>Post</button>
+    </div>
+);
 }
 
 export default Post;

d81bfeabba690ae3d127d5dd217557a18991cec1
arinaihara <aiharaarin@gmail.com>
2023-10-14 11:18:59 -0700

    minor frontend changes for profile

--- src/Components/Profiles/Profiles.tsx
+++ src/Components/Profiles/Profiles.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ProfilePokemon from './ProfilePokemon'
+import './Profiles.css'
 
 function Profiles() {
   return (
     <div>
-        <div>
+        <div className='profile-info'>
+            <h1 className='profile-name'>Name</h1>
             <form className='photo-form' encType='multipart/form-data'>
+              <p>Choose a picture to update your profile with!</p>
               <input type='file' name='image' className='inputfile' />
             </form>
         </div>

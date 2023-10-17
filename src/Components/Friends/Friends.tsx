09160369bcf8a5d6c31a291452716a074b6250da
andres <andres192@revatrure.net>
2023-10-16 21:00:07 -0600

    started friends list frontend

---
+++ src/Components/Friends/Friends.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Friends() {
+
+
+
+
+
+
+
+  return (
+    <>
+      <div>Friends</div>
+
+
+
+    <button type="button" name="get-friends">Get your friends list</button>
+
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default Friends

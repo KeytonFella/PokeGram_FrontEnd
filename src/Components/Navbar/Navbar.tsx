996a36f707ea94c55d416cc02dea3d74f006e23f
KeytonFella <keytonfriske@gmail.com>
2023-10-23 10:27:04 -0500

    no more spam

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -29,7 +29,7 @@ function Navbar() {
       .then(response => setProfile(response.data))
       .catch(err => console.log(err))
     }
-  }, [authState.token, profile])
+  }, [authState.token])
    
   return (
       <nav className="navbar sticky-top navbar-expand-xl navbar-light bg-light" id='navbar'>
